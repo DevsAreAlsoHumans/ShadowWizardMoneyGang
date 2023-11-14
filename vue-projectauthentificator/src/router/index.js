ca1e22cab15a356bd58a6b37f2fcf2f4d03d11fa
[Cyprien] <[cyprien.bocquet@gmail.com]>
2023-11-14 16:56:53 +0100

    ajout semblant de front

--- vue-projectauthentificator/src/router/index.js
+++ vue-projectauthentificator/src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createWebHistory, createRouter } from "vue-router"
+import MainHub from "@/views/MainHubPage.vue"
+import Redirect from "@/components/RedirectMain.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "redirect",
+    component: Redirect,
+  },
+  {
+    path: "/mainHub",
+    name: "mainHub",
+    component: MainHub,
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
---
+++ vue-projectauthentificator/src/views/MainHubPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <MainHub />
+    <div>
+    </div>
+</template>
+
+
+<script>
+import MainHub from "@/components/MainHubComponent.vue"
+
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+
+    },
+    components: {
+        MainHub,
+    }
+}
+</script>
